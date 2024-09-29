feea56eb7737963a92f3f5892ba75e1336db46f6
JMTC-dev <jackmatthewcasey@gmail.com>
2024-09-29 21:06:12 +1000

    feat: implement examples from lectures

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
 const App = () => {
-  console.log("Hello from component");
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+
   return (
     <div>
-      <p>Hello world</p>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(" ")}</p>
     </div>
   );
 };
