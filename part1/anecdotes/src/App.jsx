09e4b73261c0de37b4beff06e62794ba5ad14f6e
JMTC-dev <jackmatthewcasey@gmail.com>
2024-09-30 17:09:35 +1000

    feat: implement exercise 1.12, 1.13, 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
   const anecdotes = [
     "If it hurts, do it more often.",
@@ -13,8 +17,55 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [highestVote, setHighestVote] = useState(0);
+  const [highestVoteIdx, setHighestVoteIdx] = useState([]);
+  const handleAnecdoteClick = () => {
+    const randomNumber = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomNumber);
+  };
 
-  return <div>{anecdotes[selected]}</div>;
+  const filteredQuotes = anecdotes.filter((_, index) =>
+    highestVoteIdx.includes(index)
+  );
+
+  const handleVoteClick = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+
+    const newHighestVoteIdx = [];
+    const newHighestVote = newVotes.reduce((a, b) => Math.max(a, b), -Infinity);
+    newVotes.forEach((item, index) =>
+      item === newHighestVote ? newHighestVoteIdx.push(index) : null
+    );
+
+    setHighestVoteIdx(newHighestVoteIdx);
+    setHighestVote(newHighestVote);
+    setVotes(newVotes);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Anecdote of the day</h1>
+        {anecdotes[selected]}
+        <p>has {votes[selected]} votes</p>
+        <div>
+          <Button handleClick={handleVoteClick} text="vote" />
+          <Button handleClick={handleAnecdoteClick} text="next anecdote" />
+        </div>
+      </div>
+      <div>
+        <h1>Anecdote with the most votes</h1>
+        {filteredQuotes.map((quote, index) => (
+          <>
+            <p key={index}>{quote}</p>
+            <p>has {highestVote} votes</p>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
