31548535b46f8f6f78804c48d7eab4aeaa3e03e1
JMTC-dev <jackmatthewcasey@gmail.com>
2024-09-30 11:48:48 +1000

    feat: complete example project

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,4 +1,16 @@
 import { useState } from "react";
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
@@ -6,21 +18,23 @@ const App = () => {
 
   const handleLeftClick = () => {
     setAll(allClicks.concat("L"));
-    setLeft(left + 1);
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
   };
 
   const handleRightClick = () => {
     setAll(allClicks.concat("R"));
-    setRight(right + 1);
+    const updatedRight = right + 1;
+    setRight(updatedRight);
   };
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
       {right}
-      <p>{allClicks.join(" ")}</p>
+      <History allClicks={allClicks} />
     </div>
   );
 };
