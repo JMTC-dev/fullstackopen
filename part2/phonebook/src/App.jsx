b6689ce70dd25cd0fa5701c88e180ef3e5334fe4
JMTC-dev <jackmatthewcasey@gmail.com>
2024-10-02 20:08:07 +1000

    feat: implement exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,16 +2,34 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
+  const [filteredPersons, setFilteredPersons] = useState(persons);
 
   const handleNameInput = (event) => {
     setNewName(event.target.value);
   };
+
   const handleNumberInput = (event) => {
     setNewNumber(event.target.value);
+  };
+
+  const handleSearchInput = (event) => {
+    const searchValue = event.target.value;
+    setSearch(searchValue);
+    const newPersons = persons.filter((person) =>
+      person.name
+        .trim()
+        .toLowerCase()
+        .includes(searchValue.trim().toLowerCase())
+    );
+    setFilteredPersons(newPersons);
   };
 
   const addPerson = (event) => {
@@ -27,11 +45,16 @@ const App = () => {
     };
     setPersons(persons.concat(newPerson));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={handleSearchInput} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameInput} />
@@ -44,8 +67,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
+      {filteredPersons.map((person) => (
+        <p key={person.id}>
           {person.name} {person.number}
         </p>
       ))}
