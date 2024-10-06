d2ec2e0ed3dbf65dae52f617998dfa108a24419e
JMTC-dev <jackmatthewcasey@gmail.com>
2024-10-06 11:02:37 +1000

    feat: add css to the part2 example

--- part2/examples/src/App.jsx
+++ part2/examples/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
 import noteService from "./services/notes";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("some error happened...");
 
   useEffect(() => {
     console.log("effect");
@@ -28,9 +30,12 @@ const App = () => {
     const returnedNote = await noteService
       .update(id, changedNote)
       .catch((error) => {
-        alert(
-          `the note '${note.content}' was already deleted from the server'`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server'`
         );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
     if (returnedNote)
@@ -54,6 +59,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
